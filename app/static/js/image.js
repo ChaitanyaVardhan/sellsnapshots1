11226b509a21e453b9297d111e0ce0db5705785f
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2017-07-03 19:16:35 +0000

    Code for navigation of images

--- app/static/js/image.js
+++ app/static/js/image.js
@@ -8,8 +8,12 @@ function showImage(e) {
 	var src = t.getAttribute('src');
 	var imageId = t.getAttribute('id');
 	var t1 = document.getElementById('imageDIV');
-	var t2 = t1.getElementsByTagName('IMG')[1];
+	var t2 = t1.getElementsByTagName('IMG')[3];
 	t2.src = src;
+        var leftNav = t1.getElementsByTagName('IMG')[1];
+        leftNav.src = "https://s3.ap-south-1.amazonaws.com/sellsnapshots/assets/nav-arrow-left.png";        
+        var rightNav = t1.getElementsByTagName('IMG')[2];
+        rightNav.src = "https://s3.ap-south-1.amazonaws.com/sellsnapshots/assets/nav-arrow-right.png";
 //	var t3 = t1.getElementsByTagName('a')[0];
 //	var href = t3.getAttribute('href');
 //	t3.href = '/buy/' + imageId;
