c04a1d818e62b70a23095e88776b26ce50da6296
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2017-10-03 11:41:42 +0000

    upload to s3

--- app/static/js/s3_upload.js
+++ app/static/js/s3_upload.js
@@ -1,3 +1,31 @@
+function uploadFile(response, file) {
+    var postData = new FormData();
+    for (key in response.data.fields) {
+        postData[key] = response.data.fields[key]
+    }
+    postData['file'] = file;
+    console.log('calling s3');
+    console.log(postData);
+    console.log(response.data.url);
+
+    $.ajax({
+      url: response.data.url,
+      method: "POST",
+      data: postData,
+      dataType: 'json',
+      processData: false,
+      contentType: 'multipart/form-data'
+    })
+    .success(function(response) {
+      console.log('response from s3');
+      console.log(response);
+    })
+    .error(function() {
+      console.log(arguments);
+    });
+
+}
+
 function getSignedRequest(file) {
     console.log("Hello World from getSignedRequest");
     var data = {};
@@ -10,6 +38,7 @@ function getSignedRequest(file) {
         contentType: 'application/x-www-form-urlencoded; charset=UTF-8'
     }).done(function(response) {
         console.log(response);
+        uploadFile(response, file);
     });
 
 }
